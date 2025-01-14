92e0a6c3a080e9b84a6b6c3bdbca64f0102a38b5
syed hassan <s.hassankamran68@gmail.com>
2025-01-14 17:20:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front-end-developer", "Graphic Designer","Freelancer"],
+        strings: ["Front-end-developer", "UI/UX Designer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front-end-developer", "Graphic Designer","Freelancer"],
+        strings: ["Front-end-developer", "UI/UX Designer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
